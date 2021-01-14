2addc9094e3045d1d9da454b3726926794cc54d2
holbs <75672492+willholbrook7@users.noreply.github.com>
2021-01-14 10:35:34 +0000

    Hokemon_R Folder Upload

---
+++ Hokemon_R/Hokemon_R/Hokemon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Hokemon_R
+{
+    public class Hokemon
+    {
+        //MEMBERS
+
+
+        //VARIABLES
+        private string name;
+        private int health;
+        private int max_health;
+        private int attack;
+        private int speed;
+        private int defense;
+
+
+        //PROPERTIES
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+
+        //METHODS
+
+        public Hokemon()   //CONSTRUCTOR METHOD
+        {
+            provide_name();
+            max_health = 100;
+            health = random_int_generator(10, max_health);
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+
+        }
+
+
+        public void provide_name()
+        {
+            Console.WriteLine("Please enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+
+        public void get_details()
+        {
+            Console.WriteLine("\n\nName: {0}\nHealth: {1}/{2}\nAttack: {3}\nSpeed: {4}\nDefense: {5} ", name, health, max_health, attack, speed, defense);
+        }   //GET DETAILS METHOD
+
+
+        public int random_int_generator(int min_value, int max_value) //PARAMETERS
+        {
+            Random rnd = new Random(); // INSTANTIATING the rnd object from the RANDOM classes
+
+            int randomValue;
+
+            randomValue = rnd.Next(min_value, max_value);
+
+            return randomValue;
+        }
+
+    }
+}
---
+++ Hokemon_R/Hokemon_R/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hokemon_R
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of Pokemon!");
+
+            // INSTANTIATING out first object (first Hokemon)
+
+            Hokemon HokeObject01 = new Hokemon();
+
+            HokeObject01.get_details();
+
+            System.Threading.Thread.Sleep(500); // Pauses for half a second
+
+            // Create a second Hokemon
+
+            Hokemon Hoke02 = new Hokemon();
+
+            Hoke02.get_details();
+
+
+        }
+    }
+}
